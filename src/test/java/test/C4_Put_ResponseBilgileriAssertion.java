2fe59586360862f642ac9596e854865dfb41de11
Erman Coskun <115878270+ermancoskun@users.noreply.github.com>
2023-04-06 14:11:52 +0300

    v2

---
+++ src/test/java/test/C4_Put_ResponseBilgileriAssertion.java
@@ -0,0 +1,21 @@
+package test;
+
+public class C4_Put_ResponseBilgileriAssertion {
+
+    /*
+    https://jsonplaceholder.typicode.com
+    /posts/70 url’ine asagidaki Json formatindaki body ile
+    bir PUT request gonderdigimizde
+    {
+    "title":
+    title":"
+    "body":
+    body":"
+    "userId":
+    10,
+    "id":
+    70
+    }
+     */
+
+}
